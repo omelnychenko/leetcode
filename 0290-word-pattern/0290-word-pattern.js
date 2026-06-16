b51f87c581bb7996fbc81e1623d3938cdb63cbea
omelnychenko <79713335+omelnychenko@users.noreply.github.com>
2026-06-16 10:30:10 +0200

    Time: 0 ms (100%), Space: 54.1 MB (23.11%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.js
+++ 0290-word-pattern/0290-word-pattern.js
@@ -4,11 +4,15 @@
  * @return {boolean}
  */
 var wordPattern = function(pattern, s) {
-    if (pattern.length !== s.split(' ').length) return false;
-    for (const i in pattern) {
-        if ([...new Set(pattern)].indexOf(pattern[i]) !== [...new Set(s.split(' '))].indexOf(s.split(' ')[i])) {
-            return false;
-        }
+    const words = s.split(' ');
+    if (pattern.length !== words.length) return false;
+    const c2w = new Map(), w2c = new Map();
+    for (let i = 0; i < pattern.length; i++) {
+        const c = pattern[i], w = words[i];
+        if (c2w.has(c) && c2w.get(c) !== w) return false;
+        if (w2c.get(w) && w2c.get(w) !== c) return false;
+        c2w.set(c, w);
+        w2c.set(w, c);
     }
     return true;
 };
